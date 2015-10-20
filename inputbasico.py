5129000263326ea25a4cdd58ae8b24ac06b36150
George dos Santos <fredweb@globo.com>
2015-10-20 08:47:18 -0300

    inputbasico

---
+++ inputbasico.py
@@ -0,0 +1,9 @@
+"""
+Faça um Programa que peça um número e então mostre a mensagem O número informado foi [número].
+"""
+
+# -*- coding: UTF-8 -*-
+
+numero = int(input("Digite um numero:"))
+
+print('O número informado foi ' + str(numero))
