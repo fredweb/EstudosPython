59998bc8572d5e9166bd4ef1054f526bdd0ee64b
George dos Santos <fredweb@globo.com>
2015-10-20 08:53:22 -0300

    mediaBasico

---
+++ mediaBasico.py
@@ -0,0 +1,13 @@
+"""
+Faça um Programa que peça as 4 notas bimestrais e mostre a média.
+"""
+# -*- coding: UTF-8 -*-
+
+nota1 = int(input("Digite a Primeira nota:"))
+nota2 = int(input("Digite a Segunda nota:"))
+nota3 = int(input("Digite a Terceira nota:"))
+nota4 = int(input("Digite a Quarta nota:"))
+
+media = (nota1+nota2+nota3+nota4)/4
+
+print('A Media é  ' + str(media))
