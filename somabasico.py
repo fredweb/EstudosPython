ab7aa5d3c1fd3b1dd756ea4acf525bf4b0270d55
George dos Santos <fredweb@globo.com>
2015-10-20 08:49:53 -0300

    somabasico

---
+++ somabasico.py
@@ -0,0 +1,13 @@
+
+"""
+Faça um Programa que peça dois números e imprima a soma..
+"""
+
+# -*- coding: UTF-8 -*-
+
+primeiroNumero = int(input("Digite o Primeiro numero:"))
+segundoNumero = int(input("Digite o Segundo numero:"))
+
+soma = primeiroNumero + segundoNumero
+
+print('A soma é  ' + str(soma))
